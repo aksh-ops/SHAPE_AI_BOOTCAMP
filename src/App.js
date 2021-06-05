1cf252de76c2514271d7a70b6e05bff8d8280033
harsh kumar <77266966+aksh-ops@users.noreply.github.com>
2021-06-05 10:59:24 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <html>
+     <header>
+
+    <h1>ShapeAI</h1>
+    </header>
+    <body>
+    <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+<link href="https://fonts.googleapis.com/css2?family=McLaren&display=swap" rel="stylesheet"></link>
+        
+        <div className="harsh">
+        <div className="note">
+          <h1>JavaScript and React.js</h1>
+          <p>This was an amazing class taken by Shaurya Sinha.We have covered everything including html css react bootstrap etc.  </p>
+         </div>
+         
+        <div className="note">
+          <h1>JavaScript and React.js</h1>
+          <p>This was an amazing class taken by Shaurya Sinha .We have covered everything including html css react bootstrap etc.  </p>
+
+        </div>
+       
+        <div className="note">
+          <h1>JavaScript and React.js</h1>
+          <p>This was an amazing class taken by Shaurya Sinha.we have covered everything including html css react bootstrap etc.  </p>
+
+        </div>
+        </div>
+        <footer>
+          <p>Thanks for joining </p>
+        </footer>
+        </body>
+      </html>
+    </div>
+    
+  );
+}
+
+export default App;
